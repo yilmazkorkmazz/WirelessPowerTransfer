31e8ea4a339cffe874a1ea04d28439d0e55717b2
yilmazkorkmazz <ylmaz1906@hotmail.com>
2018-07-10 10:37:45 +0300

    Add files via upload

---
+++ errorCheck.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   errorChech.h
+ * Author: yilobaba1905
+ *
+ * Created on 09 Temmuz 2018 Pazartesi, 04:46
+ */
+
+#ifndef ERRORCHECK_H
+#define	ERRORCHECK_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ERRORCHECK_H */
+
+bool errorOccured(void);
+
+
+void setError(void);
+
+
+void clearErrorFlag(void);
